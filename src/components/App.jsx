77aeecbad086cae48f2da839666fb17958b1df94
YuriiHaldetskyi <uragaldetskiy@ukr.net>
2023-03-11 21:49:36 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,56 @@
 import { FeedbackOptions } from 'components/FeedBackOptions/feedbackoptions';
 import { Statictics } from 'components/Statistics/Statistics';
 import { Section } from './Section/Section';
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = event => {
+    switch (event.currentTarget.name) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+      default:
+        break;
+    }
   };
 
-  onLeaveFeedback = event => {
-    const { name } = event.currentTarget;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
+  const getTotalFeedback = () => {
+    return good + bad + neutral;
   };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const Percentage = Math.round((good / total) * 100);
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onChange={this.onLeaveFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statictics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={Percentage}
-          />
-        </Section>
-      </>
-    );
-  }
+  const Percentage = () => {
+    const total = getTotalFeedback();
+    return Math.round((good / total) * 100);
+  };
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onChange={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statictics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={getTotalFeedback()}
+          positivePercentage={Percentage()}
+        />
+      </Section>
+    </>
+  );
 }
 
 App.propType = {
